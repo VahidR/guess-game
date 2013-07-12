f28a7cd61c14895eae453251b2c89b9334de4530
Vahid Rafiei <vahid.rafiei@gmail.com>
2013-07-12 18:17:26 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,111 @@
+(function(){
+
+//Game variables
+var target = 0;
+var guess = 0;
+var remained = 10;
+var made = 0;
+var gameState = "";
+var gameWon = false;
+
+//The output and output fieldss
+var output = document.querySelector("#output");
+
+//The guess button
+var guess_button = document.querySelector("button#guess");
+guess_button.style.cursor = "pointer";
+guess_button.addEventListener("click", clickHandler, false);
+
+//The reset button
+var reset_button = document.querySelector("button#reset");
+reset_button.style.cursor = "pointer";
+reset_button.addEventListener("click", resetHandler, false);
+
+//The arrows
+var guess_arrow = document.querySelector("#guess_arrow");
+var target_arrow = document.querySelector("#target_arrow");
+
+
+function resetHandler()
+{
+  window.location.reload()
+}
+
+
+function render()
+{
+  // the scale is 300px, but the floor output is in the scale of 100.
+  guess_arrow.style.left = guess * 3 + "px";
+  target_arrow.style.left = target * 3 + "px";
+}
+
+function clickHandler()
+{
+  play();
+}
+
+function play()
+{
+  target = Math.floor(Math.random() * 100); 	
+  guess = Math.floor(Math.random() * 100);  
+  
+  remained = remained - 1;
+  made = made + 1;
+  gameState =   " Guess: " + made + ", Remaining: " + remained + 
+				"<br>"+
+				"Your guess: " + guess + ", Target: " + target;
+  
+    
+
+  if(guess > target)
+  {
+    output.innerHTML = "Your guess was too high!" + "<br>" + gameState;
+    
+    if (remained < 1)
+    {
+      end();
+    }
+  }
+  else if(guess < target)
+  {
+    output.innerHTML = "Your guess was too low!" + "<br>" + gameState;
+    
+    if (remained < 1)
+    {
+      end();
+    }
+  }
+  else if(guess === target)
+  {
+    gameWon = true;
+    end();
+  }
+  
+  //render the graphic display
+  render();
+}
+
+function end()
+{
+  if (gameWon)
+  {
+    output.innerHTML
+      = "Yes, it was " + target + "!" + 
+      "<br>" + 
+      "It only took you " + made + " guesses.";
+  }
+  else
+  {
+    output.innerHTML
+      = "No more guesses left!" + 
+      "<br>" + 
+      "The number was: " + target + " !";
+  }
+  
+  //disable the button in the end of game.
+  guess_button.removeEventListener("click", clickHandler, false);
+  guess_button.disabled = true;
+
+}
+
+}());
